064de1e68ef5cb08780888b5f9aac5672ed1fde2
mpeniket <131975856+mpeniket@users.noreply.github.com>
2024-02-13 11:31:53 +0000

    Add OpenAI API key and integrate OpenAI client

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import render_template, redirect, request, Flask
 from utils.utils import upload_file_to_folder
+<<<<<<< Updated upstream
 import whisper
 from utils.utils import openai_chat_completion
 from uploads.transcription import transcription
 
+=======
+from openai import OpenAI
+
+client = OpenAI()
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 
@@ -17,13 +23,24 @@ def index():
 # This endpoint will be used to upload the audio file and send to OpenAI speech-to-text API
 @app.route("/upload", methods=["POST"])
 def upload_file():
-
+    
     uploaded_filepath = upload_file_to_folder(request.files)
+<<<<<<< Updated upstream
 
     model = whisper.load_model("base")
     options = whisper.DecodingOptions(language="en")
     result = model.transcribe(uploaded_filepath, verbose=True, **options)
 
+=======
+    
+    audio_file= open(uploaded_filepath, "rb")
+    transcript = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file,
+    response_format="text")
+    print(transcript.words)
+    
+>>>>>>> Stashed changes
     return redirect("/")
 
 
