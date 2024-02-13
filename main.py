9d62285d50c8135b6764789b2a06686069bcc8c4
anchit-chandran <anchit97123@gmail.com>
2024-02-13 10:07:28 +0000

    Remove print statement and add file upload functionality
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, request, Flask
 from utils.utils import upload_file_to_folder
+import whisper
 
 app = Flask(__name__)
 
@@ -13,8 +14,6 @@ def index():
 # This endpoint will be used to upload the audio file and send to OpenAI speech-to-text API
 @app.route("/upload", methods=["POST"])
 def upload_file():
-
-    print(request.files)
 
     uploaded_filepath = upload_file_to_folder(request.files)
     
